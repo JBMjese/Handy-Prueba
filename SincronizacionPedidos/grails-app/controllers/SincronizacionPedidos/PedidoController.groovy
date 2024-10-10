bc8e81424e8affd320c5901e44087779fad83955
JBMjese <6935@holbertonstudents.com>
2024-10-10 10:09:47 -0500

    📜 Implementacion de logger en el servicio y controlador para registrar actividades en un archivo de log

--- SincronizacionPedidos/grails-app/controllers/SincronizacionPedidos/PedidoController.groovy
+++ SincronizacionPedidos/grails-app/controllers/SincronizacionPedidos/PedidoController.groovy
@@ -3,8 +3,12 @@ package SincronizacionPedidos
 import groovy.json.JsonSlurper
 import groovyx.net.http.RESTClient
 import grails.gorm.transactions.Transactional
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 class PedidoController {
+
+    private static final Log log = LogFactory.getLog(PedidoController)
 
     @Transactional
     def testApi() {
@@ -16,9 +20,7 @@ class PedidoController {
             def client = new RESTClient(apiUrl)
             def response = client.get(headers: ['Authorization': apiToken])
 
-
-            // para depuración
-            println "Respuesta completa de la API: ${response.data}"
+            log.debug("Respuesta completa de la API: ${response.data}")
 
             // Verificar si la respuesta es exitosa
             if (response.status == 200) {
@@ -27,18 +29,17 @@ class PedidoController {
 
                 if (responseData) {
                     // Analizar la respuesta JSON
-                    //def orders = new JsonSlurper().parseText(responseText)
-                    render "Pedidos obtenidos: ${responseData.salesOrders}"
+                    log.info("Pedidos obtenidos: ${responseData.salesOrders}")render "Pedidos obtenidos: ${responseData.salesOrders}"
                 } else {
-                    render 'La respuesta de la API está vacía.'
+                    log.info('La respuesta de la API está vacía.')
                 }
             } else {
-                render "Error al consumir la API: ${response.status} - ${response.data?.text ?: 'Respuesta nula'}"
+                log.error("Error al consumir la API: ${response.status} - ${response.data?.text ?: 'Respuesta nula'}")
             }
         } catch (Exception e) {
             // Manejo de excepciones
-            render "Error al consumir la API: ${e.message}"
-            e.printStackTrace() // Para ver más detalles en la consola
+            log.error("Error al consumir la API: ${e.message}", e)
         }
     }
+
 }
--- SincronizacionPedidos/grails-app/services/SincronizacionPedidos/PedidoService.groovy
+++ SincronizacionPedidos/grails-app/services/SincronizacionPedidos/PedidoService.groovy
@@ -1,14 +1,18 @@
 package SincronizacionPedidos
 
 import grails.gorm.transactions.Transactional
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
+
 
 @Transactional
 class PedidoService {
+    private static final Log log = LogFactory.getLog(PedidoService)
 
     void saveOrders(List<Map> orders) {
         orders.each { orderData ->
             if (!orderData.id || orderData.totalSales == null) {
-            logActivity("Datos de pedido incompletos: ${orderData}")
+            log.warn("Datos de pedido incompletos: ${orderData}")
             return // Salir si los datos son incompletos
             }
             // Busca el pedido por ID o crea uno nuevo si no existe
@@ -18,9 +22,9 @@ class PedidoService {
             
             // Guarda el pedido en la base de datos
             if (order.save(flush: true)) {
-                logActivity("Pedido ${order.id} guardado exitosamente.")
+                log.info("Pedido ${order.id} guardado exitosamente.")
             } else {
-                logActivity("Error al guardar el pedido ${order.id}: ${order.errors}")
+                log.error("Error al guardar el pedido ${order.id}: ${order.errors}")
             }
         }
     }
